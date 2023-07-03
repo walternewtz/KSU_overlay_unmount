a871505b65dc37babe4647c3f222dc2c99a50c59
Takeda-senpai <deadboltsx303@gmail.com>
2023-07-03 19:16:09 +0700

    mount writeable hosts to global mount ns

--- magisk-module/service.sh
+++ magisk-module/service.sh
@@ -3,4 +3,6 @@ chmod 755 "$MODDIR/ksuhide"
 chmod 755 "$MODDIR/magic-mount"
 rm -rf /cache/ksu_umount.log.bak
 mv -f /cache/ksu_umount.log /cache/ksu_umount.log.bak
+# mount writeable hosts to global mount ns
+mount --bind "$MODDIR/system/etc/hosts" /system/etc/hosts
 exec "$MODDIR/ksuhide"
